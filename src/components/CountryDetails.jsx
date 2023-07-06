f594d5ed25fa8a806ecde589b8014cfcd7503570
Kentliel <kent.iluminati@gmail.com>
2023-07-06 16:31:02 +0200

    the flags are enlarged

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -22,20 +22,63 @@ const CountryDetails = ({ countries, info }) => {
   if (newCountryInfo.length !== 0) {
 
     return (
-      <div id="info" className=" m-5 ">
-        <img src={`https://flagpedia.net/data/flags/icon/72x54/${newCountryInfo.alpha2Code.toLowerCase()}.png`} alt="countryFlag" />
-        <h3>{newCountryInfo.name.common}</h3>
-        <div className='data'>
-          <p>Capital: {newCountryInfo.capital}</p>
+      <div id="info" className="container m-5 ">
+        <div className='row w-50 justify-content-ms-center '>
+          <img src={`https://flagpedia.net/data/flags/icon/72x54/${newCountryInfo.alpha2Code.toLowerCase()}.png`} alt="countryFlag" />
+          <h3>{newCountryInfo.name.common}</h3>
+          <div className='data col'>
+            <p>Capital: {newCountryInfo.capital}</p>
+          </div>
+          <div className='col'>
+            <p>Area: {newCountryInfo.area}</p>
+          </div>
+          <div className='col'>
+            <p>Borders :
+              <ul>
+                <li>
+                  {newCountryInfo.borders.map(country => allCountries.map(borderCountry => {
+
+                    if (borderCountry.alpha3Code === country) {
+
+                      return (
+
+                        <p>
+                          <Link onClick={() => displayNewCountry(borderCountry)}>{borderCountry.name.common}</Link>
+
+                        </p>
+                      )
+                    }
+                  }))}
+                </li>
+              </ul>
+            </p>
+
+          </div>
+
+
+
         </div>
-        <div>
-          <p>Area: {newCountryInfo.area}</p>
-        </div>
-        <div>
-          <p>Borders :
+      </div>
+    )
+  }
+  else if (countryInfo.length !== 0) {
+
+    return (
+      <div id="info" className=" m-5 " >
+        <div className='row justify-content-ms-center '>
+          <img src={`https://flagpedia.net/data/flags/icon/72x54/${countryInfo.alpha2Code.toLowerCase()}.png`} alt="countryFlag" />
+          <h3>{countryInfo.name.common}</h3>
+          <div className='data col'>
+            <p>Capital: {countryInfo.capital}</p>
+          </div>
+          <div className='col'>
+            <p>Area: {countryInfo.area}</p>
+          </div>
+          <div className='col'>
+            <p>Borders: </p>
             <ul>
               <li>
-                {newCountryInfo.borders.map(country => allCountries.map(borderCountry => {
+                {countryInfo.borders.map(country => countries.map(borderCountry => {
 
                   if (borderCountry.alpha3Code === country) {
 
@@ -50,49 +93,8 @@ const CountryDetails = ({ countries, info }) => {
                 }))}
               </li>
             </ul>
-          </p>
-
+          </div>
         </div>
-
-
-
-
-      </div>
-    )
-  }
-  else if (countryInfo.length !== 0) {
-
-    return (
-      <div id="info" className=" m-5 " >
-        <img src={`https://flagpedia.net/data/flags/icon/72x54/${countryInfo.alpha2Code.toLowerCase()}.png`} alt="countryFlag" />
-        <h3>{countryInfo.name.common}</h3>
-        <div className='data'>
-          <p>Capital: {countryInfo.capital}</p>
-        </div>
-        <div>
-          <p>Area: {countryInfo.area}</p>
-        </div>
-        <div>
-          <p>Borders</p>
-          <ul>
-            <li>
-              {countryInfo.borders.map(country => countries.map(borderCountry => {
-
-                if (borderCountry.alpha3Code === country) {
-
-                  return (
-
-                    <p>
-                      <Link onClick={() => displayNewCountry(borderCountry)}>{borderCountry.name.common}</Link>
-
-                    </p>
-                  )
-                }
-              }))}
-            </li>
-          </ul>
-        </div>
-
       </div >
     )
 
